e7b4783f9a450f8d3c982400a4654cf8b1b04c36
chrxtina <christina.ad.nguyen@gmail.com>
2016-10-02 16:19:15 -0400

    Insert API host localhost3000

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,11 @@
 'use strict';
 
+// const app = {
+//   host: 'https://peaceful-reef-97358.herokuapp.com',
+// };
+
 const app = {
-  host: 'https://peaceful-reef-97358.herokuapp.com',
+  host: 'http://localhost:3000',
 };
 
 module.exports = app;
