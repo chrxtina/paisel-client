5ea4337457db8896ab8bd17be0ae538ec8791ab8
chrxtina <christina.ad.nguyen@gmail.com>
2016-11-18 18:20:48 -0500

    Ran grunt build

--- bundle.js
+++ bundle.js
@@ -1956,16 +1956,31 @@ webpackJsonp([0],[
 	  document.getElementById("poll-question").innerHTML = data.question;
 
 	  document.getElementById("answer-A").innerHTML = data.answers[0];
-	  document.getElementById("prog-bar-A").innerHTML = data.percent.a * 100 + "%";
-	  $("#prog-bar-A").css('width', data.percent.a * 100 + "%");
+	  if (data.percent.a) {
+	    document.getElementById("prog-bar-A").innerHTML = data.percent.a * 100 + "%";
+	    $("#prog-bar-A").css('width', data.percent.a * 100 + "%");
+	  } else {
+	    document.getElementById("prog-bar-A").innerHTML = "0%";
+	    $("#prog-bar-A").css('width', "0%");
+	  }
 
 	  document.getElementById("answer-B").innerHTML = data.answers[1];
-	  document.getElementById("prog-bar-B").innerHTML = data.percent.b * 100 + "%";
-	  $("#prog-bar-B").css('width', data.percent.b * 100 + "%");
+	  if (data.percent.b) {
+	    document.getElementById("prog-bar-B").innerHTML = data.percent.b * 100 + "%";
+	    $("#prog-bar-B").css('width', data.percent.b * 100 + "%");
+	  } else {
+	    document.getElementById("prog-bar-B").innerHTML = "0%";
+	    $("#prog-bar-B").css('width', "0%");
+	  }
 
 	  document.getElementById("answer-C").innerHTML = data.answers[2];
-	  document.getElementById("prog-bar-C").innerHTML = data.percent.c * 100 + "%";
-	  $("#prog-bar-C").css('width', data.percent.c * 100 + "%");
+	  if (data.percent.c) {
+	    document.getElementById("prog-bar-C").innerHTML = data.percent.c * 100 + "%";
+	    $("#prog-bar-C").css('width', "0%");
+	  } else {
+	    document.getElementById("prog-bar-C").innerHTML = "0%";
+	    $("#prog-bar-C").css('width', "0%");
+	  }
 
 	  $('#pollDataModal').modal('show');
 	};
